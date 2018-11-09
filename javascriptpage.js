9f15a4426296637445155fbc802954c240349b05
jeromerix <44801176+jeromerix@users.noreply.github.com>
2018-11-09 16:56:37 +0100

    Add files via upload

--- javascriptpage.js
+++ javascriptpage.js
@@ -1,7 +1,21 @@
+function myfunction() {
+    //id checkbox    
+    var checkBox= document.getElementById("checkb");
+    //id waar je iets mee wilt doen
+    var imgA    = document.getElementById("logob");
+   
+ 
+  if (checkBox.checked == true){
+    imgA.style.display  = "block";
+ 
+  } else {
+    imgA.style.display = "none";
+  }
+}
 /* contact verstuur button */
-function foo() {
+/*function foo() {
    alert("Email verstuurd!");
    return true;
-}
+}*/
 
 /*klik en verberg functie*/
